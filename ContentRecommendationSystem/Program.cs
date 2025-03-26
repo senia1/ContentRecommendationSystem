ff33f67d10be3dadbf84972fec99685bcf33320e
Senia <105051256+senia1@users.noreply.github.com>
2025-03-26 19:08:55 +0300

    added database connection

--- ContentRecommendationSystem/Program.cs
+++ ContentRecommendationSystem/Program.cs
@@ -1,2 +1,16 @@
 ﻿
-Console.WriteLine("ContentRecommendationSystem is created");
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+string connString = configuration.GetConnectionString("DefaultConnection");
+
+using (var conn = new NpgsqlConnection(connString))
+{
+    conn.Open();
+    Console.WriteLine("Подключение установлено.");
+}
